530fb9dd00aecc1b7a2d5f04d8754c1d4803b6e9
Tomy-ch <chikatomy4656@gmail.com>
2025-07-05 19:10:45 +0900

    add-label-init-command

---
+++ .makefiles/label-setting.mk
@@ -0,0 +1,19 @@
+.PHONY: reset-labels ## すべてのラベルを削除（既存ラベル含む）
+.PHONY: delete-all-labels ## .github/settings/labels.json に基づいてラベルを作成
+
+delete-all-labels:
+	@echo "🗑 既存のラベルを削除します..."
+	@gh label list --limit 1000 --json name -q '.[].name' | while read -r label; do \
+		echo "🔸 delete label: $$label"; \
+		gh label delete "$$label" --yes; \
+	done
+
+create-default-labels:
+	@echo "🏷 ラベルを作成します..."
+	@jq -c '.[]' .github/settings/labels.json | while read -r label; do \
+		name=$$(echo $$label | jq -r .name); \
+		desc=$$(echo $$label | jq -r .description); \
+		color=$$(echo $$label | jq -r .color); \
+		echo "🔸 create label: $$name"; \
+		gh label create "$$name" --description "$$desc" --color "$$color" || echo "⚠️ $$name already exists"; \
+	done
