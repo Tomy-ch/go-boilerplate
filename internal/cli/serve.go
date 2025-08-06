0fa3ca9ff30d9d5aaccabf9df111dac935c03f66
Tomy-ch <toyo.chika.fuku@gmail.com>
2025-08-07 00:41:41 +0900

    feat/add-echo-recover-middleware-and-improve-error-handling

--- internal/cli/serve.go
+++ internal/cli/serve.go
@@ -4,6 +4,7 @@ import (
 	"boilerplate-go/internal/bootstrap"
 	"boilerplate-go/internal/controller/handler/health"
 	"boilerplate-go/internal/controller/handler/healthz"
+	"boilerplate-go/internal/controller/middleware"
 	"boilerplate-go/internal/controller/middleware/logging"
 	"boilerplate-go/internal/controller/server"
 
@@ -31,9 +32,10 @@ func serveRun(_ *cobra.Command, _ []string) error {
 		logger.Fatal("failed to load config", zap.Error(err))
 	}
 
-	e := server.New(cfg, logger)
+	e := server.New()
 	health.BindHandler(e)
 	healthz.BindHandler(e)
+	middleware.UseMiddlewares(e, cfg, logger)
 
 	if err := e.Start(":8080"); err != nil {
 		logger.Fatal("called main", zap.Error(err))
