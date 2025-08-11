03f45994deded14259ade388eebfd0c29aa07871
Tomy-ch <toyo.chika.fuku@gmail.com>
2025-08-12 04:11:28 +0900

    feat: リクエストIDを取得するミドルウェアを実装し、エラーハンドラで参照できるように修正

---
+++ internal/controller/middleware/requestid/request_id.go
@@ -0,0 +1,19 @@
+// Package requestid は、リクエストIDミドルウェアをラップするためのパッケージです。
+package requestid
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+// Middleware は、リクエストIDを生成するミドルウェアを返します。
+func Middleware() echo.MiddlewareFunc {
+	return middleware.RequestID()
+}
+
+// GetRequestIDFromResponse は、レスポンスのEchoコンテキストからリクエストIDを取得します。
+func GetRequestIDFromResponse(
+	c echo.Context,
+) string {
+	return c.Response().Header().Get(echo.HeaderXRequestID)
+}
