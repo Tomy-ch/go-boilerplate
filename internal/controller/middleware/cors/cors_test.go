c7b0591757d695fcbb7ada85f2c6ebe12b8c2ef4
Tomy-ch <toyo.chika.fuku@gmail.com>
2025-08-12 10:20:36 +0900

    feat: Cookieミドルウェアの追加とCORS設定の拡張
    - Cookieミドルウェアを実装し、CSRF対策のためのクッキー制御を追加
    - CORSミドルウェアのExposeHeadersに新しいヘッダーを追加し、AllowCredentialsのデフォルト値を`false`に変更
    - URI制御ミドルウェアを`uri`ディレクトリに移動し、関連コードを更新
    - ドキュメントを更新し、各ミドルウェアの詳細と設定項目を記載

--- internal/controller/middleware/cors/cors_test.go
+++ internal/controller/middleware/cors/cors_test.go
@@ -16,11 +16,28 @@ func Test_buildCORSConfig(t *testing.T) {
 
 		allowedOrigins := []string{"http://localhost:3000", "http://localhost:4000"}
 		expected := middleware.CORSConfig{
-			AllowOrigins:     allowedOrigins,
-			AllowMethods:     []string{echo.GET, echo.POST, echo.PUT, echo.PATCH, echo.DELETE, echo.OPTIONS},
-			AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
-			ExposeHeaders:    []string{echo.HeaderContentLength},
-			AllowCredentials: true,
+			AllowOrigins: allowedOrigins,
+			AllowMethods: []string{
+				echo.HEAD,
+				echo.GET,
+				echo.POST,
+				echo.PUT,
+				echo.PATCH,
+				echo.DELETE,
+				echo.OPTIONS,
+			},
+			AllowHeaders: []string{
+				echo.HeaderOrigin,
+				echo.HeaderContentType,
+				echo.HeaderAccept,
+				echo.HeaderAuthorization,
+			},
+			ExposeHeaders: []string{
+				echo.HeaderContentDisposition,
+				echo.HeaderLocation,
+				echo.HeaderXRequestID,
+			},
+			AllowCredentials: false,
 		}
 
 		actual := buildCORSConfig(allowedOrigins)
