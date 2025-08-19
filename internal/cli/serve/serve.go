6d6acce59121e32073224a74ce7c85aaaa6b0dac
Tomy-ch <toyo.chika.fuku@gmail.com>
2025-08-19 12:06:20 +0900

    Feat: サーバー起動をfxでできるように変更

--- internal/cli/serve/serve.go
+++ internal/cli/serve/serve.go
@@ -2,22 +2,11 @@
 package serve
 
 import (
-	"boilerplate-go/internal/config"
-	"boilerplate-go/internal/controller/handler/health"
-	"boilerplate-go/internal/controller/handler/healthz"
-	v1users "boilerplate-go/internal/controller/handler/v1/users"
-	"boilerplate-go/internal/controller/middleware"
-	"boilerplate-go/internal/controller/middleware/binder"
-	"boilerplate-go/internal/controller/middleware/errorhandler"
-	"boilerplate-go/internal/controller/middleware/ipextractor"
-	"boilerplate-go/internal/controller/middleware/logging"
-	"boilerplate-go/internal/controller/middleware/validator"
-	"boilerplate-go/internal/controller/server"
-	"boilerplate-go/internal/infrastructure/rdb"
+	"boilerplate-go/internal/di"
 
 	"github.com/spf13/cobra"
 
-	"go.uber.org/zap"
+	"go.uber.org/fx"
 )
 
 // NewServeCommand は、サーバーを起動するためのコマンドを生成します。
@@ -32,34 +21,13 @@ func NewServeCommand() *cobra.Command {
 
 // serveRun は、サーバーを起動するための実行関数です。
 func serveRun(_ *cobra.Command, _ []string) error {
-	logger := logging.NewProductionLogger()
-
-	cfg, err := config.SetUpConfig()
-	if err != nil {
-		logger.Fatal("failed to load config", zap.NamedError("config", err))
-	}
-
-	validator := validator.NewValidator()
-	binder := binder.NewBinder()
-	ipextractor := ipextractor.NewIPExtractor(cfg)
-	httpErrorHandler := errorhandler.NewHTTPErrorHandler(logger)
-
-	e := server.New(cfg, validator, binder, ipextractor, httpErrorHandler)
-	middleware.UseMiddlewares(e, cfg, logger)
-
-	db, err := rdb.NewDB(cfg)
-	if err != nil {
-		logger.Fatal("failed to connect to database", zap.NamedError("database", err))
-	}
-
-	health.BindHandler(e)
-	healthz.BindHandler(e)
-
-	v1users.BindHandler(e, db)
-
-	if err := e.Start(":8080"); err != nil {
-		logger.Fatal("called main", zap.Error(err))
-	}
+	fx.New(
+		di.ConfigModule(),
+		di.DatabaseModule(),
+		di.LoggingModule(),
+		di.HTTPStackModule(),
+		di.ServeModule(),
+	).Run()
 
 	return nil
 }
--- internal/controller/server/server.go
+++ internal/controller/server/server.go
@@ -2,52 +2,51 @@
 package server
 
 import (
+	"context"
+	"strconv"
+	"time"
+
 	"boilerplate-go/internal/config"
+	"boilerplate-go/internal/controller/handler"
 
 	"github.com/labstack/echo/v4"
+	"go.uber.org/fx"
+	"go.uber.org/zap"
 )
 
-func New(
-	cfg *config.Config,
-	validator echo.Validator,
-	binder echo.Binder,
-	ipextractor echo.IPExtractor,
-	httpErrorHandler echo.HTTPErrorHandler,
-) *echo.Echo {
-	e := echo.New()
-
-	setPrimitiveEchoSettings(e, cfg)
-	setCustomEchoBindings(e, validator, binder, ipextractor, httpErrorHandler)
-
-	return e
+// New は、サーバーインスタンスを作成します。
+func New() *echo.Echo {
+	return echo.New()
 }
 
-// setPrimitiveEchoSettings は、Echoの基本的なプロパティを設定します。
-func setPrimitiveEchoSettings(
-	e *echo.Echo,
-	cfg *config.Config,
+// ServeHTTP は、HTTPサーバーを起動します。
+func ServeHTTP(
+	lc fx.Lifecycle, e *echo.Echo, cfg *config.Config, z *zap.Logger,
+	// handler.RouteRegistered は、ルート登録が完了したことを示すトークンです。
+	_ *handler.RouteMounted,
 ) {
-	isProduction := cfg.IsAppProductionMode()
-	isDevelopment := cfg.IsAppDevelopmentMode()
-
-	// 開発モード向けのデバッグ支援機能(詳細なエラーメッセージ表示)
-	e.Debug = isDevelopment
-	// バナーは本番環境では非表示にする
-	e.HideBanner = isProduction
-	// ポート番号は本番環境では非表示にする
-	e.HidePort = isProduction
-}
-
-// setCustomEchoBindings は、Echoにカスタムバインディングを設定します。
-func setCustomEchoBindings(
-	e *echo.Echo,
-	validator echo.Validator,
-	binder echo.Binder,
-	ipextractor echo.IPExtractor,
-	httpErrorHandler echo.HTTPErrorHandler,
-) {
-	e.Validator = validator
-	e.Binder = binder
-	e.IPExtractor = ipextractor
-	e.HTTPErrorHandler = httpErrorHandler
+	lc.Append(fx.Hook{
+		OnStart: func(_ context.Context) error {
+			addr := cfg.ServerPort()
+			go func() {
+				if err := e.Start(":" + strconv.Itoa(addr)); err != nil {
+					z.Error("failed to start http server", zap.Error(err))
+				}
+			}()
+			z.Info("http started",
+				zap.String("port", strconv.Itoa(addr)),
+				zap.Strings("allowed origins", cfg.AllowedOrigins()),
+				zap.String("cidr", cfg.CIDR().IP.String()),
+				zap.String("mode", cfg.ServerAppMode()),
+			)
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			shutdownTime := cfg.ServerShutdownTimeout()
+			ctx, cancel := context.WithTimeout(ctx, shutdownTime*time.Second)
+			defer cancel()
+			z.Info("http stopping")
+			return e.Shutdown(ctx)
+		},
+	})
 }
