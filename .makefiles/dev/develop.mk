69274abefc48ef34eae9b3b144ef107cde0b1edb
Tomy-ch <toyo.chika.fuku@gmail.com>
2025-11-27 12:45:34 +0900

    Feat: fmtコマンドの追加とlefthookの調整

--- .makefiles/dev/develop.mk
+++ .makefiles/dev/develop.mk
@@ -4,6 +4,7 @@
 .PHONY: tools ## 開発ツールの起動
 .PHONY: install ## goツールのインストール
 .PHONY: tidy-lib ## ローカルライブラリの依存関係更新
+.PHONY: fmt ## コードをフォーマット
 .PHONY: fix ## コードの自動修正
 .PHONY: lint ## コードの静的解析
 .PHONY: test ## CI用のテスト実行
@@ -42,6 +43,11 @@ tools:
 tidy-lib:
 	go mod tidy
 	go mod vendor
+
+fmt:
+	@echo "🔄 コードのフォーマットを実行します..."
+	@go fmt ./...
+	@echo "✅ コードのフォーマットが完了しました。"
 
 fix:
 	@golangci-lint run --fix --config .golangci-full.yaml
