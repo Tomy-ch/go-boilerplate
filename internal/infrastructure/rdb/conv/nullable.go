cd54df229641ffb6fb99b350fa40749f7eb06ab2
Tomy-ch <toyo.chika.fuku@gmail.com>
2025-12-21 02:24:04 +0900

    Feat: DBの型変換にuuid系を追加

--- internal/infrastructure/rdb/conv/nullable.go
+++ internal/infrastructure/rdb/conv/nullable.go
@@ -4,7 +4,39 @@ package conv
 import (
 	"database/sql"
 	"time"
+
+	"boilerplate-go/internal/apperror"
+	"boilerplate-go/pkg/uuid"
+	"boilerplate-go/pkg/xerrors"
+
+	googleUUID "github.com/google/uuid"
 )
+
+// UUIDPtrFromNull は、googleUUID.NullUUIDをポインタに変換します。
+func UUIDPtrFromNull(nu googleUUID.NullUUID) (*uuid.UUID, error) {
+	if !nu.Valid {
+		return nil, nil
+	}
+
+	parsedUUID, err := uuid.Parse(nu.UUID.String())
+	if err != nil {
+		return nil, xerrors.Wrap(apperror.ErrInternal, err.Error())
+	}
+	return &parsedUUID, nil
+}
+
+// NullUUIDFromPtr は、ポインタをgoogleUUID.NullUUIDに変換します。
+func NullUUIDFromPtr(u *uuid.UUID) googleUUID.NullUUID {
+	if u == nil {
+		return googleUUID.NullUUID{Valid: false}
+	}
+	return NewNullUUID(*u)
+}
+
+// NewNullUUID は、引数をgoogleUUID.NullUUIDに変換します。
+func NewNullUUID(u uuid.UUID) googleUUID.NullUUID {
+	return googleUUID.NullUUID{UUID: googleUUID.UUID(u.Bytes()), Valid: true}
+}
 
 // StringPtrFromNull は、sql.NullStringをポインタに変換します。
 func StringPtrFromNull(ns sql.NullString) *string {
@@ -19,7 +51,7 @@ func NullStringFromPtr(s *string) sql.NullString {
 	if s == nil {
 		return sql.NullString{Valid: false}
 	}
-	return sql.NullString{String: *s, Valid: true}
+	return NewNullString(*s)
 }
 
 // NewNullString は、引数をsql.NullStringに変換します。
@@ -40,7 +72,7 @@ func NullInt16FromPtr(p *int16) sql.NullInt16 {
 	if p == nil {
 		return sql.NullInt16{Valid: false}
 	}
-	return sql.NullInt16{Int16: *p, Valid: true}
+	return NewNullInt16(*p)
 }
 
 // NewNullInt16 は、引数をsql.NullInt16に変換します。
@@ -61,7 +93,7 @@ func NullInt64FromPtr(p *int64) sql.NullInt64 {
 	if p == nil {
 		return sql.NullInt64{Valid: false}
 	}
-	return sql.NullInt64{Int64: *p, Valid: true}
+	return NewNullInt64(*p)
 }
 
 // NewNullInt64 は、引数をsql.NullInt64に変換します。
@@ -82,7 +114,7 @@ func NullBoolFromPtr(p *bool) sql.NullBool {
 	if p == nil {
 		return sql.NullBool{Valid: false}
 	}
-	return sql.NullBool{Bool: *p, Valid: true}
+	return NewNullBool(*p)
 }
 
 // NewNullBool は、引数をsql.NullBoolに変換します。
@@ -103,7 +135,7 @@ func NullFloat64FromPtr(p *float64) sql.NullFloat64 {
 	if p == nil {
 		return sql.NullFloat64{Valid: false}
 	}
-	return sql.NullFloat64{Float64: *p, Valid: true}
+	return NewNullFloat64(*p)
 }
 
 // NewNullFloat64 は、引数をsql.NullFloat64に変換します。
@@ -124,7 +156,7 @@ func NullTimeFromPtr(p *time.Time) sql.NullTime {
 	if p == nil {
 		return sql.NullTime{Valid: false}
 	}
-	return sql.NullTime{Time: *p, Valid: true}
+	return NewNullTime(*p)
 }
 
 // NewNullTime は、引数をsql.NullTimeに変換します。
