f1c5f8d0c4e33f29d6adc134aaf77710ec945bed
Tomy-ch <toyo.chika.fuku@gmail.com>
2025-12-21 02:57:05 +0900

    Feat: handlertestを削除して、各種テスト用のインスタンスを直接呼び出すように実装を修正

--- internal/integration/health_test.go
+++ internal/integration/health_test.go
@@ -4,16 +4,19 @@ import (
 	"net/http"
 	"testing"
 
-	"boilerplate-go/internal/controller/handler/handlertest/testinstance"
 	"boilerplate-go/internal/controller/handler/health"
 	"boilerplate-go/internal/controller/handler/health/gen"
+	"boilerplate-go/internal/observability"
+
+	"github.com/labstack/echo/v4"
 )
 
 func TestHealth_Integration(t *testing.T) {
 	t.Parallel()
 
 	t.Run("GET /healthのエンドポイントが正常に動作することを確認する", func(t *testing.T) {
-		e, _, tf, _ := testinstance.NewTestInstanceForBindHandler(t)
+		e := echo.New()
+		tf := observability.NewNoopTracerFactory(t)
 
 		health.BindHandler(e, tf)
 
