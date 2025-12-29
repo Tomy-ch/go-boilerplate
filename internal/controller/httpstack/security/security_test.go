e117e0c9179f8988699e3b0fcb539a88fd5c439d
Tomy-ch <toyo.chika.fuku@gmail.com>
2025-12-29 18:58:46 +0900

    Feat: 読み込んだセキュリティヘッダーでセキュリティミドルウェアを構築するように実装

--- internal/controller/httpstack/security/security_test.go
+++ internal/controller/httpstack/security/security_test.go
@@ -5,53 +5,29 @@ import (
 
 	"boilerplate-go/internal/config"
 
-	"github.com/labstack/echo/v4/middleware"
 	"github.com/stretchr/testify/require"
 )
 
 func TestBuildSecureConfig(t *testing.T) {
 	t.Parallel()
-	t.Run("本番モード", func(t *testing.T) {
-		t.Parallel()
-		appCfg := config.NewApplicationConfig(config.MockConfigForTest(t))
-		appCfg.SetServerAppMode(t, config.ProductionMode)
+	cfg := config.MockConfigForTest(t)
+	secCfg := config.NewSecurityConfig(cfg)
 
-		actual := buildSecureConfig(appCfg)
+	scfg := buildSecureConfig(secCfg)
 
-		expected := middleware.SecureConfig{
-			XSSProtection:         "",
-			ContentTypeNosniff:    "nosniff",
-			XFrameOptions:         "DENY",
-			ReferrerPolicy:        "no-referrer",
-			HSTSExcludeSubdomains: false,
-			HSTSMaxAge:            31536000,
-		}
-
-		require.Equal(t, expected, actual)
-	})
-
-	t.Run("非本番モード", func(t *testing.T) {
-		t.Parallel()
-		appCfg := config.NewApplicationConfig(config.MockConfigForTest(t))
-		appCfg.SetServerAppMode(t, config.DevelopmentMode)
-
-		actual := buildSecureConfig(appCfg)
-
-		expected := middleware.SecureConfig{
-			XSSProtection:      "",
-			ContentTypeNosniff: "nosniff",
-			XFrameOptions:      "DENY",
-			ReferrerPolicy:     "no-referrer",
-		}
-
-		require.Equal(t, expected, actual)
-	})
+	require.Equal(t, secCfg.ContentTypeNosniff(), scfg.ContentTypeNosniff)
+	require.Equal(t, secCfg.ReferrerPolicy(), scfg.ReferrerPolicy)
+	require.Equal(t, secCfg.XFrameOptions(), scfg.XFrameOptions)
+	require.Equal(t, int(secCfg.HSTSMaxAge().Seconds()), scfg.HSTSMaxAge)
+	require.Equal(t, secCfg.HSTSExcludeSubdomains(), scfg.HSTSExcludeSubdomains)
+	require.Equal(t, secCfg.HSTSPreloadEnabled(), scfg.HSTSPreloadEnabled)
 }
 
 func TestMiddleware(t *testing.T) {
 	t.Parallel()
 
-	appCfg := config.NewApplicationConfig(config.MockConfigForTest(t))
+	cfg := config.MockConfigForTest(t)
+	secCfg := config.NewSecurityConfig(cfg)
 
-	require.NotNil(t, Middleware(appCfg))
+	require.NotNil(t, Middleware(secCfg))
 }
