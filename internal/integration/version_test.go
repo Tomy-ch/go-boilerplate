0e4f9f521e909f247bff68d7e72c7176e56cab22
Tomy-ch <toyo.chika.fuku@gmail.com>
2025-12-30 15:04:56 +0900

    Fix: NewOperationSystemConfigの修正漏れを修正

--- internal/integration/version_test.go
+++ internal/integration/version_test.go
@@ -25,7 +25,7 @@ func TestVersionIntegration(t *testing.T) {
 		bi := system.NewBuildInfo()
 		cfg := config.MockConfigForTest(t)
 		appCfg := config.NewApplicationConfig(cfg)
-		osCfg := config.NewOSConfig(cfg)
+		osCfg := config.NewOperationSystemConfig(cfg)
 
 		loc, err := time.LoadLocation(osCfg.TimeZone())
 		require.NoError(t, err)
