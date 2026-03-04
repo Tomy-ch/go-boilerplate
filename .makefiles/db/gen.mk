09f2664078ae104924de970c709256e78d744707
Tomy-ch <toyo.chika.fuku@gmail.com>
2026-03-05 03:38:35 +0900

    Feat: db-schema-artifacts-checkに変更

--- .makefiles/db/gen.mk
+++ .makefiles/db/gen.mk
@@ -1,15 +1,25 @@
 ## DBに対する生成コマンド群
 # -----Dockerコンテナ内で実行するターゲット-----
-.PHONY: db-schema ## スキーマの更新を実行
+.PHONY: gen-db-schema ## DBスキーマの生成を実行
+.PHONY: gen-db-schema-ci ## DBスキーマの生成を実行（CI用）
 .PHONY: dump-schema ## スキーマのダンプを実行
 # ----CI用ターゲット-----
 .PHONY: dump-schema-ci ## スキーマのダンプを実行（CI用）
 
 # -----Dockerコンテナ内で実行するターゲット-----
-db-schema:
+gen-db-schema:
 	@echo "🔄 スキーマの更新を実行します..."
 	docker compose run --rm er_diagram_generator
 	@echo "✅ スキーマの更新が完了しました。"
+
+gen-db-schema-ci:
+	docker run --rm \
+		--network host \
+		-u $(id -u):$(id -g) \
+		-v $(PWD)/docs/db-schema:/output \
+		-v $(PWD)/docker/database/schemaspy/schemaspy-ci.properties:/schemaspy.properties \
+		schemaspy/schemaspy:latest \
+		-configFile /schemaspy.properties
 
 dump-schema:
 	@echo "🔄 スキーマのダンプを実行します..."
