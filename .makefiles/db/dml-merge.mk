181af7a9fd99e4f80f993e21c5e2d25f93568e37
Tomy-ch <toyo.chika.fuku@gmail.com>
2026-03-15 01:58:37 +0900

    Fix: command_serviceの表記誤りを修正

--- .makefiles/db/dml-merge.mk
+++ .makefiles/db/dml-merge.mk
@@ -3,14 +3,14 @@
 .PHONY: merge-dml ## DMLのマージを実行
 .PHONY: merge-dml-repo ## ドメイン用DMLのマージ
 .PHONY: merge-dml-qs ## クエリサービス用DMLのマージ
-.PHONY: merge-dml-commandq ## コマンドクエリ用DMLのマージ
+.PHONY: merge-dml-cs ## コマンドサービス用DMLのマージ
 .PHONY: merge-dml-sysq ## システムクエリ用DMLのマージ
 .PHONY: merge-dml-core ## 指定したタイプのDMLのマージを実行 (例: make merge-dml-core type="repository" work-dir="/app")
 # -----CI用ターゲット-----
 .PHONY: merge-dml-ci ## DMLのマージを実行（CI用）
 .PHONY: merge-dml-ci-repo ## ドメイン用DMLのマージ（CI用）
 .PHONY: merge-dml-ci-qs ## クエリサービス用DMLのマージ（CI用）
-.PHONY: merge-dml-ci-commandq ## コマンドクエリ用DMLのマージ（CI用）
+.PHONY: merge-dml-ci-cs ## コマンドサービス用DMLのマージ（CI用）
 .PHONY: merge-dml-ci-sysq ## システムクエリ用DMLのマージ（CI用）
 .PHONY: merge-dml-ci-core ## 指定したタイプのDMLのマージを実行（CI用） (例: make merge-dml-ci-core type="repository" work-dir="/app")
 
@@ -19,15 +19,15 @@ merge-dml:
 	make merge-dml-repo
 	make merge-dml-qs
 	make merge-dml-sysq
-	make merge-dml-commandq
+	make merge-dml-cs
 merge-dml-repo:
 	make merge-dml-core type="repository" work-dir="/app"
 merge-dml-qs:
 	make merge-dml-core type="query_service" work-dir="/app"
 merge-dml-sysq:
 	make merge-dml-core type="system_query" work-dir="/app"
-merge-dml-commandq:
-	make merge-dml-core type="command_query" work-dir="/app"
+merge-dml-cs:
+	make merge-dml-core type="command_service" work-dir="/app"
 merge-dml-core:
 	@echo "🔄 DMLのマージを実行します... (type=$(type) work-dir=$(work-dir))"
 	@docker compose run --rm go_tool_runner make merge-dml-ci-core type="$(type)" work-dir="$(work-dir)"
@@ -38,14 +38,14 @@ merge-dml-ci:
 	make merge-dml-ci-repo work-dir=$(work-dir)
 	make merge-dml-ci-qs work-dir=$(work-dir)
 	make merge-dml-ci-sysq work-dir=$(work-dir)
-	make merge-dml-ci-commandq work-dir=$(work-dir)
+	make merge-dml-ci-cs work-dir=$(work-dir)
 merge-dml-ci-repo:
 	make merge-dml-ci-core type="repository" work-dir=$(work-dir)
 merge-dml-ci-qs:
 	make merge-dml-ci-core type="query_service" work-dir=$(work-dir)
 merge-dml-ci-sysq:
 	make merge-dml-ci-core type="system_query" work-dir=$(work-dir)
-merge-dml-ci-commandq:
-	make merge-dml-ci-core type="command_query" work-dir=$(work-dir)
+merge-dml-ci-cs:
+	make merge-dml-ci-core type="command_service" work-dir=$(work-dir)
 merge-dml-ci-core:
 	go run cmd/main.go merge-dml --type=$(type) --work-dir=$(work-dir)
