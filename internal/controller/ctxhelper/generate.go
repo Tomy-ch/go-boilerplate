3176c87a4153b4e787839c565b883df8f8994b43
Tomy-ch <toyo.chika.fuku@gmail.com>
2026-03-29 17:35:51 +0900

    Feat: ctxhelperで再生成

---
+++ internal/controller/ctxhelper/generate.go
@@ -0,0 +1,4 @@
+// Package ctxhelper is provides utilities for securely maintaining metadata during request processing for Echo and the standard context.Context.
+package ctxhelper
+
+//go:generate go run ../../../scripts/genctxkey --name authn --type boilerplate-go/internal/usecase/boundary/auth.Authn --out .
