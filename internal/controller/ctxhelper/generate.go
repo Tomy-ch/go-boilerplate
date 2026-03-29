a8acd26bd2cc28a7a2a031fb64fac488d72d961e
Tomy-ch <toyo.chika.fuku@gmail.com>
2026-03-29 18:24:16 +0900

    Fix: Generateコマンドの引数を更新し、importパスを追加

--- internal/controller/ctxhelper/generate.go
+++ internal/controller/ctxhelper/generate.go
@@ -1,4 +1,4 @@
 // Package ctxhelper provides utilities for securely maintaining metadata during request processing for Echo and the standard context.Context.
 package ctxhelper
 
-//go:generate go run ../../../scripts/genctxkey --name authn --type boilerplate-go/internal/usecase/boundary/auth.Authn --out .
+//go:generate go run ../../../scripts/genctxkey --name Authn --type "auth.Authn" --import boilerplate-go/internal/usecase/boundary/auth --out .
