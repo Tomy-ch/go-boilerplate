901bd1f645967e578ea6c8dff79cc0f00fbf70e5
Tomy-ch <toyo.chika.fuku@gmail.com>
2026-03-29 17:55:04 +0900

    Fix: ctxhelperパッケージのコメント文法を修正

--- internal/controller/ctxhelper/generate.go
+++ internal/controller/ctxhelper/generate.go
@@ -1,4 +1,4 @@
-// Package ctxhelper is provides utilities for securely maintaining metadata during request processing for Echo and the standard context.Context.
+// Package ctxhelper provides utilities for securely maintaining metadata during request processing for Echo and the standard context.Context.
 package ctxhelper
 
 //go:generate go run ../../../scripts/genctxkey --name authn --type boilerplate-go/internal/usecase/boundary/auth.Authn --out .
