8b0cc852aeb0cc8bfe0097fff3a50a67bfca8f49
Tomy-ch <toyo.chika.fuku@gmail.com>
2026-04-03 02:50:16 +0900

    Gen: クエリのマージコマンドを実行

--- database/gen/user_query_service.gen.sql
+++ database/gen/user_query_service.gen.sql
@@ -1,13 +1,45 @@
 
--- === source: database/dml/query_service/user/select_users_by_keyword.sql ===
--- name: ListUsersByKeywords :many
-SELECT sqlc.embed(u)
+-- === source: database/dml/query_service/user/count_user_by_keyword.sql ===
+-- name: CountSearchUsers :one
+SELECT COUNT(*)
 FROM users AS u
-WHERE CASE sqlc.arg('active_state')
-        WHEN 'active' THEN u.deleted_at IS NULL
-        WHEN 'deleted' THEN u.deleted_at IS NOT NULL
-        ELSE TRUE
-    END
-    AND u.search_text ILIKE ALL(sqlc.arg('patterns_param')::TEXT [])
+WHERE u.search_text ILIKE ALL(sqlc.arg('patterns_param')::TEXT []);
+
+-- name: CountSearchActiveUsers :one
+SELECT COUNT(*)
+FROM users AS u
+WHERE u.search_text ILIKE ALL(sqlc.arg('patterns_param')::TEXT [])
+    AND u.deleted_at IS NULL;
+
+-- name: CountSearchDeletedUsers :one
+SELECT COUNT(*)
+FROM users AS u
+WHERE u.search_text ILIKE ALL(sqlc.arg('patterns_param')::TEXT [])
+    AND u.deleted_at IS NOT NULL;
+
+-- === source: database/dml/query_service/user/select_users_by_keyword.sql ===
+-- name: SearchUsers :many
+SELECT sqlc.embed(u), p.name AS prefecqture_name
+FROM users AS u
+JOIN prefectures AS p ON u.prefecture_id = p.id
+WHERE u.search_text ILIKE ANY(sqlc.arg('patterns_param')::TEXT [])
+ORDER BY u.created_at DESC
+LIMIT sqlc.arg('limit_param') OFFSET sqlc.arg('offset_param');
+
+-- name: SearchActiveUsers :many
+SELECT sqlc.embed(u), p.name AS prefecqture_name
+FROM users AS u
+JOIN prefectures AS p ON u.prefecture_id = p.id
+WHERE u.search_text ILIKE ANY(sqlc.arg('patterns_param')::TEXT [])
+    AND u.deleted_at IS NULL
+ORDER BY u.created_at DESC
+LIMIT sqlc.arg('limit_param') OFFSET sqlc.arg('offset_param');
+
+-- name: SearchDeletedUsers :many
+SELECT sqlc.embed(u), p.name AS prefecqture_name
+FROM users AS u
+JOIN prefectures AS p ON u.prefecture_id = p.id
+WHERE u.search_text ILIKE ANY(sqlc.arg('patterns_param')::TEXT [])
+    AND u.deleted_at IS NOT NULL
 ORDER BY u.created_at DESC
 LIMIT sqlc.arg('limit_param') OFFSET sqlc.arg('offset_param');
