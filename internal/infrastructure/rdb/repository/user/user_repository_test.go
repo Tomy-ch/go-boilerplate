ba9479345bee2f976c0d3e8ea551c3227034ba7f
Tomy-ch <toyo.chika.fuku@gmail.com>
2026-04-03 10:18:49 +0900

    Fix: FindAllメソッドをFindByActiveメソッドに変更し、アクティブ状態に基づくユーザー取得機能の実装を変更

--- internal/infrastructure/rdb/repository/user/user_repository_test.go
+++ internal/infrastructure/rdb/repository/user/user_repository_test.go
@@ -30,7 +30,7 @@ func TestNew(t *testing.T) {
 	require.Equal(t, expected, actual)
 }
 
-func TestFindAll(t *testing.T) {
+func Test_repository_FindByActive(t *testing.T) {
 	t.Parallel()
 
 	loggingDB := testkit.NewTestLoggingProvider(t)
@@ -44,80 +44,129 @@ func TestFindAll(t *testing.T) {
 		db:     loggingDB,
 	}
 
+	firstUserID, err := uuid.Parse("eaabee3e-3b7a-4f61-8fa9-030944625e92")
+	require.NoError(t, err)
+
+	lastUserID, err := uuid.Parse("550e8400-e29b-41d4-a716-446655440000")
+	require.NoError(t, err)
+
 	t.Run("正常系", func(t *testing.T) {
 		t.Parallel()
 
-		t.Run("limitとoffsetを指定した場合、作成順で複数件が取得できる", func(t *testing.T) {
+		t.Run("activeがnilの場合", func(t *testing.T) {
 			t.Parallel()
 
-			txm.WithinTx(func(ctx context.Context) {
-				limit := int32(100)
-				offset := int32(0)
+			t.Run("limitとoffsetを指定した場合、作成順で複数件が取得できる", func(t *testing.T) {
+				t.Parallel()
 
-				firstUserID, err := uuid.Parse("eaabee3e-3b7a-4f61-8fa9-030944625e92")
-				require.NoError(t, err)
+				txm.WithinTx(func(ctx context.Context) {
+					limit := int32(100)
+					offset := int32(0)
 
-				lastUserID, err := uuid.Parse("550e8400-e29b-41d4-a716-446655440000")
-				require.NoError(t, err)
+					actual, err := repo.FindByActive(ctx, nil, limit, offset)
+					require.NoError(t, err)
 
-				actual, err := repo.FindAll(ctx, limit, offset)
-				require.NoError(t, err)
+					actualFirst := actual[0]
+					actualLast := actual[len(actual)-1]
 
-				actualFirst := actual[0]
-				actualLast := actual[len(actual)-1]
+					require.Equal(t, firstUserID, actualFirst.ID())
+					require.Equal(t, lastUserID, actualLast.ID())
+				})
+			})
 
-				require.Equal(t, firstUserID, actualFirst.ID())
-				require.Equal(t, lastUserID, actualLast.ID())
+			t.Run("limit=1でoffset=0の場合先頭のユーザーが取得できる", func(t *testing.T) {
+				t.Parallel()
+
+				expectedLength := 1
+
+				txm.WithinTx(func(ctx context.Context) {
+					limit := int32(1)
+					offset := int32(0)
+
+					actual, err := repo.FindByActive(ctx, nil, limit, offset)
+					require.NoError(t, err)
+					require.Len(t, actual, expectedLength)
+
+					require.Equal(t, firstUserID, actual[0].ID())
+				})
+			})
+
+			t.Run("limit=1でoffset=9の場合、末尾のユーザーが取得できる", func(t *testing.T) {
+				t.Parallel()
+
+				txm.WithinTx(func(ctx context.Context) {
+					limit := int32(1)
+					offset := int32(9)
+
+					all, getAllUsersErr := repo.FindByActive(ctx, nil, limit, offset)
+					require.NoError(t, getAllUsersErr)
+
+					actual := all[len(all)-1]
+
+					require.Equal(t, lastUserID, actual.ID())
+				})
+			})
+
+			t.Run("limit=0の場合、空配列になる", func(t *testing.T) {
+				t.Parallel()
+
+				txm.WithinTx(func(ctx context.Context) {
+					limit := int32(0)
+					offset := int32(0)
+					actual, err := repo.FindByActive(ctx, nil, limit, offset)
+					require.NoError(t, err)
+					require.Empty(t, actual)
+				})
 			})
 		})
 
-		t.Run("limit=1でoffset=0の場合先頭のユーザーが取得できる", func(t *testing.T) {
+		t.Run("activeがtrueの場合", func(t *testing.T) {
 			t.Parallel()
 
-			firstUserID, err := uuid.Parse("eaabee3e-3b7a-4f61-8fa9-030944625e92")
-			require.NoError(t, err)
-			expectedLength := 1
+			t.Run("limitとoffsetを指定した場合、作成順で複数件が取得できる", func(t *testing.T) {
+				t.Parallel()
 
-			txm.WithinTx(func(ctx context.Context) {
-				limit := int32(1)
-				offset := int32(0)
+				txm.WithinTx(func(ctx context.Context) {
+					limit := int32(100)
+					offset := int32(0)
 
-				actual, err := repo.FindAll(ctx, limit, offset)
-				require.NoError(t, err)
-				require.Len(t, actual, expectedLength)
+					actual, err := repo.FindByActive(ctx, ptr.To(true), limit, offset)
+					require.NoError(t, err)
 
-				require.Equal(t, firstUserID, actual[0].ID())
+					actualFirst := actual[0]
+					actualLast := actual[len(actual)-1]
+
+					require.Equal(t, firstUserID, actualFirst.ID())
+					require.Equal(t, lastUserID, actualLast.ID())
+				})
 			})
 		})
 
-		t.Run("limit=1でoffset=9の場合、末尾のユーザーが取得できる", func(t *testing.T) {
+		t.Run("activeがfalseの場合", func(t *testing.T) {
 			t.Parallel()
 
-			lastUserID, err := uuid.Parse("550e8400-e29b-41d4-a716-446655440000")
-			require.NoError(t, err)
+			t.Run("limitとoffsetを指定した場合、作成順で複数件が取得できる", func(t *testing.T) {
+				t.Parallel()
 
-			txm.WithinTx(func(ctx context.Context) {
-				limit := int32(1)
-				offset := int32(9)
+				txm.WithinTx(func(ctx context.Context) {
+					limit := int32(100)
+					offset := int32(0)
 
-				all, getAllUsersErr := repo.FindAll(ctx, limit, offset)
-				require.NoError(t, getAllUsersErr)
+					firstUserID, err := uuid.Parse("d711970c-8e86-4875-8a34-e90bd79096a5")
+					require.NoError(t, err)
 
-				actual := all[len(all)-1]
+					lastUserID, err := uuid.Parse("e99b0380-522c-4636-a2b6-452acdd7c4ff")
+					require.NoError(t, err)
 
-				require.Equal(t, lastUserID, actual.ID())
-			})
-		})
+					actual, err := repo.FindByActive(ctx, ptr.To(false), limit, offset)
+					require.NoError(t, err)
 
-		t.Run("limit=0の場合、空配列になる", func(t *testing.T) {
-			t.Parallel()
+					actualFirst := actual[0]
+					actualLast := actual[len(actual)-1]
 
-			txm.WithinTx(func(ctx context.Context) {
-				limit := int32(0)
-				offset := int32(0)
-				actual, err := repo.FindAll(ctx, limit, offset)
-				require.NoError(t, err)
-				require.Empty(t, actual)
+					require.Equal(t, firstUserID, actualFirst.ID())
+					require.Equal(t, lastUserID, actualLast.ID())
+				})
 			})
 		})
 	})
@@ -129,7 +178,7 @@ func TestFindAll(t *testing.T) {
 			t.Parallel()
 
 			txm.WithinTx(func(ctx context.Context) {
-				actual, err := repo.FindAll(ctx, -1, 0)
+				actual, err := repo.FindByActive(ctx, nil, -1, 0)
 				require.Nil(t, actual)
 				require.Error(t, err)
 			})
@@ -139,7 +188,7 @@ func TestFindAll(t *testing.T) {
 			t.Parallel()
 
 			txm.WithinTx(func(ctx context.Context) {
-				actual, err := repo.FindAll(ctx, 10, -1)
+				actual, err := repo.FindByActive(ctx, nil, 10, -1)
 				require.Nil(t, actual)
 				require.Error(t, err)
 			})
@@ -148,34 +197,95 @@ func TestFindAll(t *testing.T) {
 		t.Run("無効なユーザーが挿入されていてもDomain化の時にエラーになる", func(t *testing.T) {
 			t.Parallel()
 
-			txm.WithinTx(func(ctx context.Context) {
-				drv := driver.New(ctx, db)
-				_, execErr := drv.Exec(ctx,
-					"INSERT INTO users "+
-						"(id, first_name, last_name, password_hash, email, phone, prefecture_id, city, street, postal_code) "+
-						"VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10)",
-					"07e5b6d3-0000-4000-8000-000000000000",
-					"Tx",
-					"",
-					"$2a$10$dummy",
-					"tx-insert@example.com",
-					"000-000-0000",
-					"a03aaec4-3bd6-4bfb-8e47-2fbfa026d344",
-					"City",
-					"Street",
-					"000-0000",
-				)
-				require.NoError(t, execErr)
+			t.Run("activeがnilの場合", func(t *testing.T) {
+				t.Parallel()
 
-				res, actualErr := repo.FindAll(ctx, 100, 0)
-				require.Nil(t, res)
-				require.ErrorIs(t, actualErr, user.ErrInvalidLastName)
+				txm.WithinTx(func(ctx context.Context) {
+					drv := driver.New(ctx, db)
+					_, execErr := drv.Exec(ctx,
+						"INSERT INTO users "+
+							"(id, first_name, last_name, password_hash, email, phone, prefecture_id, city, street, postal_code) "+
+							"VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10)",
+						"07e5b6d3-0000-4000-8000-000000000000",
+						"Tx",
+						"",
+						"$2a$10$dummy",
+						"tx-insert@example.com",
+						"000-000-0000",
+						"a03aaec4-3bd6-4bfb-8e47-2fbfa026d344",
+						"City",
+						"Street",
+						"000-0000",
+					)
+					require.NoError(t, execErr)
+
+					res, actualErr := repo.FindByActive(ctx, nil, 100, 0)
+					require.Nil(t, res)
+					require.ErrorIs(t, actualErr, user.ErrInvalidLastName)
+				})
+			})
+
+			t.Run("activeがtrueの場合", func(t *testing.T) {
+				t.Parallel()
+
+				txm.WithinTx(func(ctx context.Context) {
+					drv := driver.New(ctx, db)
+					_, execErr := drv.Exec(ctx,
+						"INSERT INTO users "+
+							"(id, first_name, last_name, password_hash, email, phone, prefecture_id, city, street, postal_code) "+
+							"VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10)",
+						"07e5b6d3-0000-4000-8000-000000000000",
+						"Tx",
+						"",
+						"$2a$10$dummy",
+						"tx-insert@example.com",
+						"000-000-0000",
+						"a03aaec4-3bd6-4bfb-8e47-2fbfa026d344",
+						"City",
+						"Street",
+						"000-0000",
+					)
+					require.NoError(t, execErr)
+
+					res, actualErr := repo.FindByActive(ctx, ptr.To(true), 100, 0)
+					require.Nil(t, res)
+					require.ErrorIs(t, actualErr, user.ErrInvalidLastName)
+				})
+			})
+
+			t.Run("activeがfalseの場合", func(t *testing.T) {
+				t.Parallel()
+
+				txm.WithinTx(func(ctx context.Context) {
+					drv := driver.New(ctx, db)
+					_, execErr := drv.Exec(ctx,
+						"INSERT INTO users "+
+							"(id, first_name, last_name, password_hash, email, phone, prefecture_id, city, street, postal_code, deleted_at) "+
+							"VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11)",
+						"07e5b6d3-0000-4000-8000-000000000000",
+						"Tx",
+						"",
+						"$2a$10$dummy",
+						"tx-insert@example.com",
+						"000-000-0000",
+						"a03aaec4-3bd6-4bfb-8e47-2fbfa026d344",
+						"City",
+						"Street",
+						"000-0000",
+						"2024-01-01T00:00:00Z",
+					)
+					require.NoError(t, execErr)
+
+					res, actualErr := repo.FindByActive(ctx, ptr.To(false), 100, 0)
+					require.Nil(t, res)
+					require.ErrorIs(t, actualErr, user.ErrInvalidLastName)
+				})
 			})
 		})
 	})
 }
 
-func TestCreateUser(t *testing.T) {
+func Test_repository_CreateUser(t *testing.T) {
 	t.Parallel()
 
 	loggingDB := testkit.NewTestLoggingProvider(t)
@@ -268,7 +378,7 @@ func TestCreateUser(t *testing.T) {
 	})
 }
 
-func TestCountByActive(t *testing.T) {
+func Test_repository_CountByActive(t *testing.T) {
 	t.Parallel()
 
 	loggingDB := testkit.NewTestLoggingProvider(t)
